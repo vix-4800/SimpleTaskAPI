213734f39c1ff9436751aa2d6322ff0d53810535
vix-4800 <Gordan.ei@protonmail.ch>
2024-02-29 18:06:42 +0400

    user accessor registered_at added

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Facades\Date;
 use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
@@ -40,6 +42,11 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function getRegisteredAtAttribute()
+    {
+        return Date::createFromDate($this->created_at)->format('Y-m-d');
+    }
 
     public function tasks(): HasMany
     {
