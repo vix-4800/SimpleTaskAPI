4ffa5651ded49cbe0ffed696e5c05902d576bb4a
vix-4800 <Gordan.ei@protonmail.ch>
2024-02-29 14:20:15 +0400

    task factory and seeder added

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Task>
+ */
+class TaskFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'status' => $this->faker->randomElement([
+                'todo',
+                'in_progress',
+                'completed',
+            ]),
+            'due_date' => $this->faker->date(),
+        ];
+    }
+}
