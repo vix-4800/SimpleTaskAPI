ac5ff89f609f0a997a2e2286a0622229488a1eac
vix-4800 <Gordan.ei@protonmail.ch>
2024-02-29 14:20:01 +0400

    task model and migration added

---
+++ database/migrations/2024_02_29_100657_create_tasks_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->enum('status', [
+                'todo',
+                'in_progress',
+                'completed',
+            ])->default('todo');
+            $table->date('due_date')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
