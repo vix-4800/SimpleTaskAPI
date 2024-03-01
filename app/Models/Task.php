b652fe7b21d4e7ffc66c59af7404d443bffd7058
vix-4800 <Gordan.ei@protonmail.ch>
2024-03-01 19:52:59 +0400

    task model - due date accessor added

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,6 +6,7 @@ use App\Enums\TaskStatus;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Support\Facades\Date;
 
 class Task extends Model
 {
@@ -24,9 +25,18 @@ class Task extends Model
         'due_date',
     ];
 
+    protected $dates = [
+        'due_date',
+    ];
+
     protected $casts = [
         'status' => TaskStatus::class,
     ];
+
+    public function getDueDateAttribute($date)
+    {
+        return Date::createFromDate($date)->format('Y-m-d');
+    }
 
     public function user(): BelongsTo
     {
